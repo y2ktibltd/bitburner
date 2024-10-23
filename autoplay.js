4ba9d26d3cb8fc456fc11a59328e8819f8665dd0
Tom Bigham <75551001+y2ktibltd@users.noreply.github.com>
2024-10-23 16:55:14 -0400

    Update autoplay.js

--- autoplay.js
+++ autoplay.js
@@ -19,22 +19,25 @@ export async function main(ns) {
   ns.print("Attacking " + target + " with swarm");
 
   while (true) {
-    if (ns.getServerSecurityLevel(target) > minSecurity + 5) {
-      await pauseAfterScript(ns, "weak.js", home, target);
-    }
-    else if (ns.getServerMoneyAvailable(target) < maxMoney * 0.9) {
-      await pauseAfterScript(ns, "grow.js", home, target);
-    }
-    else {
-      await pauseAfterScript(ns, "hack.js", home, target);
-    }
-    while (ns.peek(69420) != "NULL PORT DATA") {
-      ns.print(ns.readPort(69420));
+    var freeRam = Math.floor(ns.getServerMaxRam(home) - ns.getServerUsedRam(home));
+    if (freeRam > 0) {
+      if (ns.getServerSecurityLevel(target) > minSecurity + 5) {
+        await runScripts(ns, "weak.js", home, target);
+      }
+      else if (ns.getServerMoneyAvailable(target) < maxMoney * 0.9) {
+        await runScripts(ns, "grow.js", home, target);
+      }
+      else {
+        await runScripts(ns, "hack.js", home, target);
+      }
+      while (ns.peek(69420) != "NULL PORT DATA") {
+        ns.print(ns.readPort(69420));
+      }
     }
   }
 }
 //WIP
-async function runScriptsSequentially(ns, script, home, target) {
+async function runScripts(ns, script, home, target) {
   switch (script) {
     case "hack.js": {
       var threads = Math.floor(ns.hackAnalyzeThreads(target, ns.getServerMoneyAvailable(target) * 0.1));
@@ -57,42 +60,19 @@ async function runScriptsSequentially(ns, script, home, target) {
       break;
     }
   }
-  var scriptRam = threads * ns.getScriptRam(script);
-  if (scriptRam < ns.getServerMaxRam(home)-ns.getServerUsedRam(home) && threads > 0) {
+  if (!ns.scriptRunning(script, home)) {
     ns.exec(script, home, threads, target);
+  } else {
+    await ns.sleep(10);
   }
 }
 
 
 async function pauseAfterScript(ns, script, home, target) {
   var threads = Math.floor((ns.getServerMaxRam(home) - ns.getServerUsedRam(home)) / ns.getScriptRam(script));
-  if (threads > 0) {
-    switch (script) {
-      case "hack.js": {
-        threads = Math.floor(ns.hackAnalyzeThreads(target, ns.getServerMoneyAvailable(target) * 0.1));
-        break;
-      }
-      case "grow.js": {
-        var grwThreads = Math.floor(ns.growthAnalyze(target, 100 - ((ns.getServerMoneyAvailable(target) / ns.getServerMaxMoney(target)) * 100), ns.getServer(home)["cpuCores"]));
-        if (grwThreads < threads) {
-          threads = grwThreads;
-        }
-        break;
-      }
-      case "weak.js": {
-        var wkThreads = Math.floor((ns.getServerSecurityLevel(target) - ns.getServerMinSecurityLevel(target)) / ns.weakenAnalyze(1, ns.getServer(home)["cpuCores"]));;
-        if (wkThreads < threads) {
-          threads = wkThreads;
-        } else if (wkThreads < 0) {
-          threads = 1;
-        }
-        break;
-      }
-    }
-    ns.exec(script, home, threads, target);
-    while (ns.scriptRunning(script, home)) {
-      await ns.sleep(50);
-    }
+  ns.exec(script, home, threads, target);
+  while (ns.scriptRunning(script, home)) {
+    await ns.sleep(50);
   }
 }
 
@@ -135,29 +115,15 @@ export function runDeploy(ns, home, script, target) {
 export function getRoot(ns, home) {
   var targets = getList(ns, home);
   for (let target of targets) {
-    if (!ns.hasRootAccess(target) && ns.getServerMaxRam(target) > 0 && target != home) {
-      if (ns.getHackingLevel() >= ns.getServer(target)["requiredHackingSkill"]) {
-        if (ns.getServer(target)["numOpenPortsRequired"] > ns.getServer(target)["openPortCount"]) {
-          if (ns.fileExists('BruteSSH.exe')) {
-            ns.brutessh(target);
-          }
-          if (ns.fileExists('FTPCrack.exe')) {
-            ns.ftpcrack(target);
-          }
-          if (ns.fileExists('relaySMTP.exe')) {
-            ns.relaysmtp(target);
-          }
-          if (ns.fileExists('HTTPWorm.exe')) {
-            ns.httpworm(target);
-          }
-          if (ns.fileExists('SQLInject.exe')) {
-            ns.sqlinject(target);
-          }
-        }
-        if (ns.getServer(target)["openPortCount"] >= ns.getServer(target)["numOpenPortsRequired"]) {
-          ns.nuke(target);
-        }
-      }
+    if (!ns.hasRootAccess(target) && (ns.getHackingLevel() >= ns.getServer(target)["requiredHackingSkill"]) && target != home) {
+      try {
+        ns.brutessh(target);
+        ns.ftpcrack(target);
+        ns.relaysmtp(target);
+        ns.httpworm(target);
+        ns.sqlinject(target);
+        ns.nuke(target);
+      } catch (Error) { }
     }
   }
 }
